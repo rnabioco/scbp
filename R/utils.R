c8403f568bb2569a80efbbca8f1fcee70980643d
Kent Riemondy <kent.riemondy@gmail.com>
2020-01-02 12:36:14 -0700

    clonotype functions

--- R/utils.R
+++ R/utils.R
@@ -56,6 +56,7 @@ discrete_palette_default <- c(tableu_classic_palatte,
 #' If false, then the minumum value for the plotted feature will be used.
 #' @param minimal_theme plot bare minimum
 #' @param dims which dims to plot from embedding, defaults to first and second, i.e. c(1,2).
+#' @export
 plot_feature <- function(seurat_obj,
                          feature = NULL,
                          plot_dat = NULL,
@@ -762,7 +763,7 @@ make_cellbrowser <- function(so,
     unlink(file.path(outdir, project, "markers.tsv"))
   }
 
-  do.call(function(...) {ExportToCellbrowser(so,
+  do.call(function(...) {ExportToCellbrowserFast(so,
                                              dir = file.path(outdir, project),
                                              dataset.name = project,
                                              reductions = embeddings,
@@ -801,3 +802,325 @@ build_cellbrowser <- function(dataset_paths,
     message(paste(c(cbBuild_path, cb_args), collapse = " "))
   }
 }
+
+
+#'  get clonotype information per cell
+#'
+#' @param vdj_dir path to clonotypes file
+#' @param prefix prefix to add to cell_id
+#' @export
+get_clonotypes <- function(vdj_dir, prefix = "") {
+  ctypes <- read_csv(file.path(vdj_dir, "filtered_contig_annotations.csv"))
+
+  ctypes <- mutate(ctypes,
+                   cell_id = str_remove(barcode, "-1$"),
+                   cell_id = str_c(prefix, cell_id)) %>%
+    select(cell_id,
+           raw_clonotype_id) %>%
+    unique()
+
+  consensus_ctypes <- read_csv(file.path(vdj_dir,
+                                         "clonotypes.csv"))
+
+  ctypes <- left_join(ctypes,
+                      consensus_ctypes,
+                      by = c("raw_clonotype_id" = "clonotype_id"))
+
+  ctypes
+}
+
+#'  add clonotype information per cell
+#'
+#' @param sobj Seurat object
+#' @param vdj_dirs path to clonotypes file
+#' @param prefixes prefixes to add to cell_id
+#' @export
+add_clonotypes <- function(sobj, vdj_dirs, prefixes = NULL) {
+
+  if(is.null(prefixes)){
+    prefixes <- rep("", length(vdj_dirs))
+  } else {
+    if(length(prefixes) != length(vdj_dirs)){
+      stop("prefixes must be the same length as the # of vdj_dirs")
+    }
+  }
+  out <- map2_dfr(vdj_dirs, prefixes, get_clonotypes)
+
+  cells <- tibble(cell_id = Cells(sobj))
+
+  res <- left_join(cells, out, by = "cell_id") %>%
+    as.data.frame() %>%
+    tibble::column_to_rownames("cell_id")
+
+  sobj <- AddMetaData(sobj, metadata = res)
+
+  sobj
+
+}
+
+
+
+
+#' Export Seurat object for UCSC cell browser
+#' optionally uses Readr instead of base R for writing to disk
+#'
+#' @param object Seurat object
+#' @param dir path to directory where to save exported files. These are:
+#' exprMatrix.tsv, tsne.coords.tsv, meta.tsv, markers.tsv and a default cellbrowser.conf
+#' @param dataset.name name of the dataset. Defaults to Seurat project name
+#' @param reductions vector of reduction names to export
+#' @param markers.file path to file with marker genes
+#' @param cluster.field name of the metadata field containing cell cluster
+#' @param cb.dir path to directory where to create UCSC cellbrowser static
+#' website content root, e.g. an index.html, .json files, etc. These files
+#' can be copied to any webserver. If this is specified, the cellbrowser
+#' package has to be accessible from R via reticulate.
+#' @param port on which port to run UCSC cellbrowser webserver after export
+#' @param skip.expr.matrix whether to skip exporting expression matrix
+#' @param skip.metadata whether to skip exporting metadata
+#' @param skip.reductions whether to skip exporting reductions
+#' @param ... specifies the metadata fields to export. To supply field with
+#' human readable name, pass name as \code{field="name"} parameter.
+#'
+#' @return This function exports Seurat object as a set of tsv files
+#' to \code{dir} directory, copying the \code{markers.file} if it is
+#' passed. It also creates the default \code{cellbrowser.conf} in the
+#' directory. This directory could be read by \code{cbBuild} to
+#' create a static website viewer for the dataset. If \code{cb.dir}
+#' parameter is passed, the function runs \code{cbBuild} (if it is
+#' installed) to create this static website in \code{cb.dir} directory.
+#' If \code{port} parameter is passed, it also runs the webserver for
+#' that directory and opens a browser.
+#'
+#' @author Maximilian Haeussler, Nikolay Markov
+#'
+#' @importFrom utils browseURL
+#' @importFrom reticulate py_module_available import
+#' @importFrom tools file_ext
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ExportToCellbrowserFast(object = pbmc_small, dataset.name = "PBMC", dir = "out")
+#' }
+#'
+ExportToCellbrowserFast <- function(
+  object,
+  dir,
+  dataset.name = Project(object = object),
+  reductions = "tsne",
+  markers.file = NULL,
+  cluster.field = "Cluster",
+  cb.dir = NULL,
+  port = NULL,
+  skip.expr.matrix = FALSE,
+  skip.metadata = FALSE,
+  skip.reductions = FALSE,
+  ...
+) {
+  vars <- c(...)
+
+  use_readr <- Seurat:::PackageCheck("readr", error = FALSE)
+
+  if (is.null(x = vars)) {
+    vars <- c("nCount_RNA", "nFeature_RNA")
+    if (length(x = levels(x = Seurat::Idents(object = object))) > 1) {
+      vars <- c(vars, cluster.field)
+      names(x = vars) <- c("", "", "ident")
+    }
+  }
+  names(x = vars) <- names(x = vars) %||% vars
+  names(x = vars) <- sapply(
+    X = 1:length(x = vars),
+    FUN = function(i) {
+      return(ifelse(
+        test = nchar(x = names(x = vars)[i]) > 0,
+        yes = names(x = vars[i]),
+        no = vars[i]
+      ))
+    }
+  )
+  if (!is.null(x = port) && is.null(x = cb.dir)) {
+    stop("cb.dir parameter is needed when port is set")
+  }
+  if (!dir.exists(paths = dir)) {
+    dir.create(path = dir)
+  }
+  if (!dir.exists(paths = dir)) {
+    stop("Output directory ", dir, " cannot be created or is a file")
+  }
+  if (dataset.name == "SeuratProject") {
+    warning("Using default project name means that you may overwrite project with the same name in the cellbrowser html output folder")
+  }
+  order <- colnames(x = object)
+  enum.fields <- c()
+  # Export expression matrix:
+  if (!skip.expr.matrix) {
+    # Relatively memory inefficient - maybe better to convert to sparse-row and write in a loop, row-by-row?
+    df <- as.data.frame(x = as.matrix(x = Seurat::GetAssayData(object = object)))
+    df <- data.frame(gene = rownames(x = object), df, check.names = FALSE)
+    gzPath <- file.path(dir, "exprMatrix.tsv.gz")
+
+    message("Writing expression matrix to ", gzPath)
+
+    if(!use_readr){
+      z <- gzfile(gzPath, "w")
+      write.table(x = df, sep = "\t", file = z, quote = FALSE, row.names = FALSE)
+      close(con = z)
+    } else {
+      readr::write_tsv(x = df, path = gzPath)
+    }
+
+  }
+  # Export cell embeddings
+  embeddings.conf <- c()
+  for (reduction in reductions) {
+    if (!skip.reductions) {
+      df <- Seurat::Embeddings(object = object, reduction = reduction)
+      if (ncol(x = df) > 2) {
+        warning(
+          'Embedding ',
+          reduction,
+          ' has more than 2 coordinates, taking only the first 2'
+        )
+        df <- df[, 1:2]
+      }
+      colnames(x = df) <- c("x", "y")
+      df <- data.frame(cellId = rownames(x = df), df)
+      fname <- file.path(dir, paste0(reduction, '.coords.tsv'))
+      message("Writing embeddings to ", fname)
+      if(!use_readr){
+        write.table(
+          x = df[order, ],
+          sep = "\t",
+          file = fname,
+          quote = FALSE,
+          row.names = FALSE
+        )
+      } else {
+        readr::write_tsv(x = df[order, ], path = fname)
+      }
+
+    }
+    conf <- sprintf(
+      '{"file": "%s.coords.tsv", "shortLabel": "Seurat %1$s"}',
+      reduction
+    )
+    embeddings.conf <- c(embeddings.conf, conf)
+  }
+  # Export metadata
+  df <- data.frame(row.names = rownames(x = object[[]]))
+  df <- Seurat::FetchData(object = object, vars = names(x = vars))
+  colnames(x = df) <- vars
+  enum.fields <- Filter(
+    f = function(name) {!is.numeric(x = df[[name]])},
+    x = vars
+  )
+  if (!skip.metadata) {
+    fname <- file.path(dir, "meta.tsv")
+    message("Writing meta data to ", fname)
+    df <- data.frame(Cell = rownames(x = df), df, check.names = FALSE)
+
+    if(!use_readr){
+      write.table(
+        x = df[order, ],
+        sep = "\t",
+        file = fname,
+        quote = FALSE,
+        row.names = FALSE
+      )
+    } else {
+      readr::write_tsv(x = df[order, ],
+                       path = fname)
+    }
+
+  }
+  # Export markers
+  markers.string <- ''
+  if (!is.null(x = markers.file)) {
+    ext <- tools::file_ext(x = markers.file)
+    fname <- paste0("markers.", ext)
+    file.copy(from = markers.file, to = file.path(dir, fname))
+    markers.string <- sprintf(
+      'markers = [{"file": "%s", "shortLabel": "Seurat Cluster Markers"}]',
+      fname
+    )
+  }
+  config <- c(
+    'name="%s"',
+    'shortLabel="%1$s"',
+    'exprMatrix="exprMatrix.tsv.gz"',
+    '#tags = ["10x", "smartseq2"]',
+    'meta="meta.tsv"',
+    '# possible values: "gencode-human", "gencode-mouse", "symbol" or "auto"',
+    'geneIdType="auto"',
+    'clusterField="%s"',
+    'labelField="%2$s"',
+    'enumFields=%s',
+    '%s',
+    'coords=%s'
+  )
+  config <- paste(config, collapse = '\n')
+  enum.string <- paste0(
+    "[",
+    paste(paste0('"', enum.fields, '"'), collapse = ", "),
+    "]"
+  )
+  coords.string <- paste0(
+    "[",
+    paste(embeddings.conf, collapse = ",\n"),
+    "]"
+  )
+  config <- sprintf(
+    config,
+    dataset.name,
+    cluster.field,
+    enum.string,
+    markers.string,
+    coords.string
+  )
+  fname <- file.path(dir, "cellbrowser.conf")
+  if (file.exists(fname)) {
+    message(
+      "`cellbrowser.conf` already exists in target directory, refusing to ",
+      "overwrite it"
+    )
+  } else {
+    cat(config, file = fname)
+  }
+  message("Prepared cellbrowser directory ", dir)
+  if (!is.null(x = cb.dir)) {
+    if (!reticulate::py_module_available(module = "cellbrowser")) {
+      stop(
+        "The Python package `cellbrowser` is required to prepare and run ",
+        "Cellbrowser. Please install it ",
+        "on the Unix command line with `sudo pip install cellbrowser` (if root) ",
+        "or `pip install cellbrowser --user` (as a non-root user). ",
+        "To adapt the Python that is used, you can either set the env. variable RETICULATE_PYTHON ",
+        "or do `require(reticulate) and use one of these functions: use_python(), use_virtualenv(), use_condaenv(). ",
+        "See https://rstudio.github.io/reticulate/articles/versions.html; ",
+        "at the moment, R's reticulate is using this Python: ",
+        reticulate::import(module = 'sys')$executable,
+        ". "
+      )
+    }
+    if (!is.null(x = port)) {
+      port <- as.integer(x = port)
+    }
+    message("Converting cellbrowser directory to html/json files")
+    cb <- reticulate::import(module = "cellbrowser")
+    cb$cellbrowser$build(dir, cb.dir)
+    if (!is.null(port)) {
+      message("Starting http server")
+      cb$cellbrowser$stop()
+      cb$cellbrowser$serve(cb.dir, port)
+      Sys.sleep(time = 0.4)
+      utils::browseURL(url = paste0("http://localhost:", port))
+    }
+  }
+}
+
+
+
+
